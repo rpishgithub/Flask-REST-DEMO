42f08983f62ba8020468d7510ba9f52a96a62ef8
unknown <rpishmail@gmail.com>
2020-10-09 19:49:26 +0800

    Models

---
+++ Models/Item.py
@@ -0,0 +1,20 @@
+from . import db, ma
+
+
+class Item(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(100), nullable=False)
+    isFinished = db.Column(db.Boolen, nullable=False)
+
+    def __init__(self, content, isFinished):
+        self.content = content
+        self.isFinished = isFinished
+
+
+class ItemSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "content", "isFinished")
+
+
+item_schema = ItemSchema()
+items_schema = ItemSchema(many=True)
